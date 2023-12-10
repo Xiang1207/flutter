69ba1dd64eacd53e3b28643e4f4f81f8869bece3
xiang1207 <kk95989598@gmail.com>
2023-12-10 10:20:54 +0800

    創建按鈕完成

--- lib/ui/add_task_bar.dart
+++ lib/ui/add_task_bar.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_work_calendar/ui/theme.dart';
+import 'package:flutter_work_calendar/ui/widgets/button.dart';
 import 'package:flutter_work_calendar/ui/widgets/input_field.dart';
 import 'package:get/get.dart';
-import 'package:intl/date_symbol_data_http_request.dart';
 import 'package:intl/intl.dart';
+import 'theme.dart';
 
-
-class AddTaskPage extends StatefulWidget{
+class AddTaskPage extends StatefulWidget {
   const AddTaskPage({Key? key}) : super(key: key);
 
   @override
@@ -14,17 +13,36 @@ class AddTaskPage extends StatefulWidget{
 }
 
 class _AddTaskPageState extends State<AddTaskPage> {
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _noteController = TextEditingController();
   DateTime _selectedDate = DateTime.now();
-  String _endTime="9:30 PM";
+  String _endTime = "9:30 PM"; // 修正這裡的拼寫錯誤
   String _startTime = DateFormat("hh:mm a").format(DateTime.now()).toString();
-  //startTime顯示目前時區 UTC 時區
+  int _selectedRemind = 5;
+  List<int> remindList=[
+    5,
+    10,
+    15,
+    20,
+  ];
+
+  int _selectedColor=0;
+
+  String _selectedRepeat = "Note";
+  List<String> repeatList=[
+    "沒有任何",
+    "日常的",
+    "每週",
+    "每月"
+  ];
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: context.theme.backgroundColor,
       appBar: _appBar(context),
       body: Container(
-        padding: const EdgeInsets.only(left: 20,right:20),
+        padding: const EdgeInsets.only(left: 20, right: 20),
         child: SingleChildScrollView(
           child: Column(
             children: [
@@ -32,14 +50,15 @@ class _AddTaskPageState extends State<AddTaskPage> {
                 "新增行程",
                 style: headingStyle,
               ),
-              MyInputField(title: "標題", hint: "請輸入標題"),
-              MyInputField(title: "事項", hint: "請輸入詳細事項"),
+              MyInputField(title: "標題", hint: "請輸入標題",controller: _titleController,),
+              MyInputField(title: "事項", hint: "請輸入詳細事項",controller: _noteController),
               MyInputField(title: "日期", hint: DateFormat.yMd().format(_selectedDate),
                 widget: IconButton(
-                  icon: Icon(Icons.calendar_today_outlined,
+                  icon: Icon(
+                    Icons.calendar_today_outlined,
                     color: Colors.grey,
                   ),
-                  onPressed: (){
+                  onPressed: () {
                     print("Hi there");
                     _getDateFromUser();
                   },
@@ -47,34 +66,93 @@ class _AddTaskPageState extends State<AddTaskPage> {
               Row(
                 children: [
                   Expanded(
-                      child: MyInputField(
-                        title: "開始日期",
-                        hint: _startTime,
-                        widget: IconButton(
-                          onPressed: (){
-                            _gerTimeFromUser(isStarTime:true);
-                          },
-                          icon: Icon(
-                            Icons.access_time_rounded,
-                            color:Colors.grey,
-                          ),
+                    child: MyInputField(
+                      title: "開始時間",
+                      hint: _startTime,
+                      widget: IconButton(
+                        onPressed: () {
+                          _gerTimeFromUser(isStarTime: true);
+                        },
+                        icon: Icon(
+                          Icons.access_time_rounded,
+                          color: Colors.grey,
                         ),
-                      )),
-                  SizedBox(width: 12,),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 12),
                   Expanded(
-                      child: MyInputField(
-                        title: "結束日期",
-                        hint: _endTime,
-                        widget: IconButton(
-                          onPressed: (){
-
-                          },
-                          icon: Icon(
-                            Icons.access_time_rounded,
-                            color:Colors.grey,
-                          ),
+                    child: MyInputField(
+                      title: "結束時間",
+                      hint: _endTime, // 修正這裡的拼寫錯誤
+                      widget: IconButton(
+                        onPressed: () {
+                          _gerTimeFromUser(isStarTime: false);
+                        },
+                        icon: Icon(
+                          Icons.access_time_rounded,
+                          color: Colors.grey,
                         ),
-                      )),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              MyInputField(title: "提醒", hint: "提早$_selectedRemind 分鐘",
+                widget: DropdownButton(
+                  icon:Icon(Icons.keyboard_arrow_down,
+                  color: Colors.grey,
+
+                  ),
+                  iconSize: 32,
+                  elevation: 4,
+                  style: subTitleStyle,
+                  underline: Container(height: 0,),
+                  onChanged: (String? newValue){
+                    setState(() {
+                      _selectedRemind = int.parse(newValue!);
+                    });
+                  },
+                  items:remindList.map<DropdownMenuItem<String>>((int value){
+                    return DropdownMenuItem<String>(
+                      value: value.toString(),
+                      child: Text(value.toString())
+                    );
+                  }
+                  ).toList(),
+                  ),
+                ),
+              MyInputField(title: "重複", hint: "$_selectedRepeat",
+                widget: DropdownButton(
+                  icon:Icon(Icons.keyboard_arrow_down,
+                    color: Colors.grey,
+
+                  ),
+                  iconSize: 32,
+                  elevation: 4,
+                  style: subTitleStyle,
+                  underline: Container(height: 0,),
+                  onChanged: (String? newValue){
+                    setState(() {
+                      _selectedRepeat = newValue!;
+                    });
+                  },
+                  items:repeatList.map<DropdownMenuItem<String>>((String? value){
+                    return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value!, style:TextStyle(color: Colors.grey))
+                    );
+                  }
+                  ).toList(),
+                ),
+              ),
+              SizedBox(height: 18,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  _colorPallete(),
+                  MyButton(label: "創建行程", onTap: ()=>_validateDate())
                 ],
               )
             ],
@@ -84,76 +162,130 @@ class _AddTaskPageState extends State<AddTaskPage> {
     );
   }
 
-  _appBar(BuildContext context){
+  _validateDate(){
+    if(_titleController.text.isNotEmpty&&_noteController.text.isNotEmpty){
+      //增加資料庫
+      Get.back();
+    }else if(_titleController.text.isEmpty ||_noteController.text.isEmpty){
+      Get.snackbar("Required", "All fields are required !",
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.white,
+        colorText: pinkClr,
+        icon: Icon(Icons.warning_amber_rounded,
+        color:Colors.red
+        )
+      );
+    }
+  }
+
+  _colorPallete(){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("Color",
+          style: titleStyle,
+        ),
+        SizedBox(height: 8.0,),
+        Wrap(
+          children: List<Widget>.generate(
+              3,
+                  (int index){
+                return GestureDetector(
+                  onTap: (){
+                    setState(() {
+                      _selectedColor=index;
+                      print("$index");
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: CircleAvatar(
+                      radius: 14,
+                      backgroundColor: index==0?primaryClr:index==1?pinkClr:yellowClr,
+                      child: _selectedColor==index?Icon(Icons.done,
+                          color: Colors.white,
+                          size: 16
+                      ):Container(),
+                    ),
+                  ),
+                );
+              }
+          ),
+        )
+      ],
+    );
+  }
+
+  _appBar(BuildContext context) {
     return AppBar(
       elevation: 0,
-      backgroundColor: Colors.transparent,//將backgroundColor設置為透明
-      //backgroundColor: context.theme.backgroundColor,
+      backgroundColor: Colors.transparent,
       leading: GestureDetector(
         onTap: () {
           Get.back();
         },
-
-        child: Icon(Icons.arrow_back_ios,
-            //左上角月亮太陽圖案變更
-            size: 20,
-            color: Get.isDarkMode? Colors.white:Colors.black
+        child: Icon(
+          Icons.arrow_back_ios,
+          size: 20,
+          color: Get.isDarkMode ? Colors.white : Colors.black,
         ),
       ),
       actions: [
         CircleAvatar(
-          backgroundImage: AssetImage(
-              "images/profile.png"
-          ),
+          backgroundImage: AssetImage("images/profile.png"),
         ),
-        SizedBox(width: 20,),
+        SizedBox(width: 20),
       ],
     );
   }
 
   _getDateFromUser() async {
-    DateTime? _pickerDate =await showDatePicker(
-        context: context,
-        initialDate: DateTime.now(),
-        firstDate: DateTime(2015),
-        lastDate: DateTime(2121)
+    DateTime? _pickerDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2015),
+      lastDate: DateTime(2121),
     );
 
-    if(_pickerDate!=null){
+    if (_pickerDate != null) {
       setState(() {
         _selectedDate = _pickerDate;
         print(_selectedDate);
       });
-    }else{
+    } else {
       print("它為空或有問題");
     }
   }
 
   _gerTimeFromUser({required bool isStarTime}) async {
     TimeOfDay? pickedTime = await _showTimePickerMethod();
-    String _formatedTime;
-    if (pickedTime == null) {
-      setState(() {});
-    } else {
-      _formatedTime = pickedTime!.format(context);
-      if (isStarTime == true) {
-        _startTime = _formatedTime;
-      } else if (isStarTime == false) {
-        _endTime = _formatedTime;
+    String _formattedTime = _startTime; // 將初始值設為開始時間
+
+    if (pickedTime != null) {
+      _formattedTime = pickedTime.format(context);
+
+      if (isStarTime) {
+        setState(() {
+          _startTime = _formattedTime;
+        });
+      } else {
+        setState(() {
+          _endTime = _formattedTime;
+        });
       }
+    } else {
+      print("時間取消");
     }
   }
 
   _showTimePickerMethod() {
     return showTimePicker(
-        initialEntryMode: TimePickerEntryMode.input,
-        context: context,
-        initialTime: TimeOfDay(
-            hour: 9,
-            minute: 10
-        )
+      initialEntryMode: TimePickerEntryMode.input,
+      context: context,
+      initialTime: TimeOfDay(
+          hour: int.parse(_startTime.split(":")[0]),
+          minute: int.parse(_startTime.split(":")[1].split(" ")[0]),
+      )
     );
   }
-
 }
-
